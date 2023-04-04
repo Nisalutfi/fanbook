4cfe81138eac37210ad84fe8af918cfe4bf0a580
Nisalutfi <nisalutfi917@gmail.com>
2023-04-05 02:28:38 +0700

    update

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ python3 -m venv $VIRTUALENV
 fi
 
 if [ ! -f $VIRTUALENV/bin/pip ]; then
-curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/pip
+curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
 
 $VIRTUALENV/bin/pip install -r requirements.txt
